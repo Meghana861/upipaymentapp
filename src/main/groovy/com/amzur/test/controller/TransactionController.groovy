23f448aaf672e72337fe362a83fcbe1ca2f22541
Meghana861 <meghaanaas2002@gmail.com>
2024-09-09 15:37:02 +0530

    add

--- src/main/groovy/com/amzur/test/controller/TransactionController.groovy
+++ src/main/groovy/com/amzur/test/controller/TransactionController.groovy
@@ -1,13 +1,14 @@
 package com.amzur.test.controller
 
 import com.amzur.test.model.TransactionModel
+import com.amzur.test.service.KafkaProducerClient
 import com.amzur.test.service.TransactionService
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
-
+import io.micronaut.http.HttpResponse
 import javax.inject.Inject
 
 
@@ -16,6 +17,9 @@ class TransactionController {
 
     @Inject
     TransactionService transactionService
+
+    @Inject
+    KafkaProducerClient kafkaProducerClient
 
     @Post
     def transferMoney(@Body TransferRequest transferRequest) {
@@ -26,7 +30,16 @@ class TransactionController {
         String upiPin = transferRequest.upiPin
 
 
-        return transactionService.transferMoney(senderMobileNumber, receiverMobileNumber, amount, upiPin)
+        def transaction=transactionService.transferMoney(senderMobileNumber, receiverMobileNumber, amount, upiPin)
+        String message = """{
+                senderMobileNumber: "${transaction.senderMobileNumber}",
+                receiverMobileNumber:" ${transaction.receiverMobileNumber}",
+                amount: "${transaction.amount}",
+                transactionDate:"${transaction.transactionDate}",
+                transactionTime:"${transaction.transactionTime}"
+        }"""
+        kafkaProducerClient.sendMessage("payment-topic", message)
+        return HttpResponse.ok([message: "Transferred Money Successfully"])
     }
     @Get("/history/{mobileNumber}")
     def getTransactionHistory(@PathVariable String mobileNumber) {
--- src/main/groovy/com/amzur/test/service/AccountService.groovy
+++ src/main/groovy/com/amzur/test/service/AccountService.groovy
@@ -17,13 +17,13 @@ class AccountService {
 
     @Transactional
     def createAccount(AccountModel accountModel) {
-        // Find the user by ID
+
         def user = UserDomain.get(accountModel.user.id)
         if (!user) {
             throw new IllegalArgumentException("User not found")
         }
 
-        // Create AccountDomain object from AccountModel
+
         def accountDomain = new AccountDomain(
                 bankName: accountModel.bankName,
                 accountNumber: accountModel.accountNumber,
@@ -37,22 +37,22 @@ class AccountService {
 
         def primaryAccount = findPrimaryAccount(user)
         if (!primaryAccount) {
-            // Set this account as primary if none exists
+
             accountDomain.isPrimary = true
         } else {
-            // Set as primary based on input, default to false
+
             accountDomain.isPrimary = accountModel.isPrimary ?: false
         }
 
-        // Save the account to the database
+
         accountDomain.save(flush: true)
 
-        // If the newly created account is set as primary, unset other primary accounts
+
         if (accountDomain.isPrimary) {
             unsetOtherPrimaryAccounts(accountDomain)
         }
 
-        // Return the created AccountModel with the saved details
+
         def accountModels = new AccountModel(
                 id: accountDomain.id,
                 bankName: accountDomain.bankName,
@@ -66,7 +66,7 @@ class AccountService {
         return accountModels
     }
 
-// Helper method to unset other primary accounts for a user
+
     private void unsetOtherPrimaryAccounts(AccountDomain newPrimaryAccount) {
         UserDomain user = newPrimaryAccount.user
         findByUser(user).findAll { it.isPrimary }.each { account ->
